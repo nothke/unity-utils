72d203b5c3dcbef87e3f70c3e0bc60e1f2dce0be
Ivan Notaros <nothke@gmail.com>
2021-06-09 16:41:20 +0200

    Added NamedArrayAttribute

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Most scripts contain description on how to use them in the header, unless the us
 * LabelDrawer.cs - Draws labels anywhere in 3D space as long as it's called.
 * BezierUtility.cs - Contains both pure functions and a struct (for ease of use), for drawing, finding tangents and closest points for cubic bezier curves. Originally derived from BezierUtility found in Unity's URP package.
 * ArrayRandomExtension.cs - Extension for getting a random element from a list or array with `array.GetRandom()`
+* NamedArrayAttribute.cs - Replaces array element labels with enum names in the inspector.
 
 ### Other utils you might like not in this repo:
 * [FlyCam](https://github.com/nothke/FlyCam) - Simple WASD flyable camera with zooming and changing speed on scroll.
---
+++ Runtime/NamedArrayAttribute.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Defines an attribute that makes the array use enum values as labels.
+// Use like this:
+//      [NamedArray(typeof(eDirection))] public GameObject[] m_Directions;
+public class NamedArrayAttribute : PropertyAttribute
+{
+    public Type TargetEnum;
+    public NamedArrayAttribute(Type TargetEnum)
+    {
+        this.TargetEnum = TargetEnum;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        // Properly configure height for expanded contents.
+        return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
+    }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Replace label with enum name if possible.
+        try
+        {
+            var config = attribute as NamedArrayAttribute;
+            var enum_names = System.Enum.GetNames(config.TargetEnum);
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            var enum_label = enum_names.GetValue(pos) as string;
+            // Make names nicer to read (but won't exactly match enum definition).
+            enum_label = ObjectNames.NicifyVariableName(enum_label.ToLower());
+            label = new GUIContent(enum_label);
+        }
+        catch
+        {
+            // keep default label
+        }
+        EditorGUI.PropertyField(position, property, label, property.isExpanded);
+    }
+}
+#endif
