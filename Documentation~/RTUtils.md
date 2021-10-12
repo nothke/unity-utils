ccc9261fdf21b3cb16c770beb095e50545da4033
Ivan Notaros <nothke@gmail.com>
2021-10-12 23:18:23 +0200

    Refactored RTUtils, added mesh and TMPText rendering

---
+++ Documentation~/RTUtils.md
@@ -0,0 +1,106 @@
+# RTUtils
+
+RenderTexture utilities for directly drawing meshes, text or textures (sprites) onto a render texture without a camera, and converting to Texture2D.
+
+![RTUtils sample](images/rtutils_example.gif)
+
+### Why?
+
+Typically, rendering to an off-screen texture in Unity is done via a second camera to which you apply a rander texture as a target. However, in the quite common case when you wish to draw simple things, like a few quads, text or a simple unlit mesh, a full Unity camera has an uncecessary overhead, which includes culling, batching, drawing shadows, sorting geometry, etc. This is especially true in HDRP, where the upfront cost of camera rendering is especially high.
+
+## How to render without a camera
+
+### Setting up
+
+To start rendering, a capture matrix needs to be setup, which defines the type and location of the "fake camera". You can do this by picking one of the following functions:
+- `rt.BeginOrthoRendering()` to start drawing an orthographic view within a 0-1 space
+- `rt.BeginPixelRendering()` to start drawing an orthographic view within a space defined by texture resolution. So, a 512x512 render texture will have positions from 0 to 512
+- `rt.BeginPerspectiveRendering()` to start drawing a perspective view, providing it fov, camera position and rotation and optionally near and far planes
+- `rt.BeginRendering()` to start drawing with a custom perspective-view matrix
+
+IMPORTANT: Once you start rendering, you must not forget to end it with `rt.EndRendering()`
+
+### Drawing
+
+In between the `rt.BeginXXXRendering()` and `rt.EndRendering()` functions, you can call any of the GL functions (for example `GL.Clear(true, true, Color.black)` to clear the texture), or you can use any of the following:
+
+#### Meshes
+
+Use `rt.DrawMesh(text, position, size)` to draw TMPText meshes. 
+
+#### Sprites
+
+You can also draw a simple texture in a rect with 
+
+#### TMP Text
+
+Use `rt.DrawTMPText(text, position, size)` to draw TMPText meshes. 
+
+Note that you will need to setup your TMPText object before you draw it:
+- The position of the text drawn on texture depends on the TMPText's pivot, anchor and alignment. For example, you might want to make a non-UI based TMPText, and put the anchor to lower left and alignment to left bottom. This will render the text from the `position` parameter and to the above-right direction;
+- TMPText's character size also matters. It will be multiplied with the `size` parameter in `DrawMesh()` function, so that you can more easily draw multiple meshes per frame with the same TMPText object;
+- If you want to set the text string right before rendering it, know that TMP will wait for the end of frame to update the actual mesh. Hence you need to manually force it to change text immediately. You can do that by using `text.ForceMeshUpdate(true, true);` after you set the text, and before you render it to the texture.
+- Note that the function will use the currently set material to TMPText. But it doesn't scale SDF-antialiasing properly, so you'll need to tweak the smoothing parameter in the SDF material to get the right antialiasing for the right font size.
+
+## Example
+
+```
+using Nothke.Utils;
+
+...
+
+rt.BeginPerspectiveRendering(60, new Vector3(0, 0, -2.5f), Quaternion.identity);
+{ // scope is not required, but looks nicer :)
+
+    GL.Clear(true, true, Color.black);
+
+    Quaternion rotation = Quaternion.Euler(0, Time.time * 100, 0);
+
+    rt.DrawMesh(monkeMesh, testMaterial,
+        Matrix4x4.TRS(new Vector3(-0.5f, 0, 0), rotation, Vector3.one));
+
+    rt.DrawMesh(monkeMesh, testMaterial,
+        Matrix4x4.TRS(new Vector3(0.5f, 0, 0), rotation, Vector3.one));
+}
+rt.EndRendering(); // Ends perspective rendering
+
+rt.BeginOrthoRendering();
+{
+    rt.DrawTMPText(text, new Vector2(0.05f, 0.95f), 0.3f);
+
+    rt.DrawSprite(texture, new Rect(0.7f, 0.1f, 0.2f, 0.2f * rt.Aspect()));
+}
+rt.EndRendering(); // Ends ortho rendering
+```
+
+### Why not use CommandBuffers?
+
+In some of my tests with CommandBuffers, they turned out to be slower to execute than just simply calling GL functions. The difference is that RTUtils functions (currently) execute on the main thread, while using the CommandBuffers executes in a separate rendering thread. However, you still need to setup the buffer in the main thread, and there is some overhead for the execution as well. So if you just want to draw a couple of meshes, calling them on the main thread turns out to be faster.
+
+However, if you wish to render a lot more meshes onto a RenderTexture, using CommandBuffers is a the way to go, but I feel that if you want to use them, you should write them yourself, here is an example to get you started:
+
+```
+using UnityEngine.Rendering;
+
+...
+
+CommandBuffer cb = new CommandBuffer();
+
+cb.SetRenderTarget(myRenderTexture);
+cb.SetProjectionMatrix(Matrix4x4.Ortho(0, 1, 0, 1, -100, 100));
+cb.ClearRenderTarget(true, true, Color.black);
+
+// You can also draw a renderer, but an automatic transform update will be run before it:
+cb.DrawRenderer(testRenderer, testMaterial);
+
+// Render a mesh:
+cb.DrawMesh(mesh, transform.localToWorldMatrix, testMaterial);
+
+// Render text:
+Matrix4x4 textMatrix = Matrix4x4.TRS(new Vector3(0, 1), Quaternion.identity, Vector3.one);
+cb.DrawMesh(text.mesh, textMatrix, text.materialForRendering, 0, 0);
+
+...
+
+Graphics.ExecuteCommandBuffer(cb);
+```
--- Runtime/RTUtils.cs
+++ Runtime/RTUtils.cs
@@ -1,14 +1,14 @@
-﻿///
+///
 /// RTUtils by Nothke
 /// 
-/// RenderTexture utilities for direct drawing sprites and converting to Texture2D.
+/// RenderTexture utilities for direct drawing meshes, texts and sprites and converting to Texture2D.
 /// Requires BlitQuad shader.
 /// 
 /// ============================================================================
 ///
 /// MIT License
 ///
-/// Copyright(c) 2021 Ivan Notaroš
+/// Copyright(c) 2021 Ivan Notaro�
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -31,12 +31,18 @@
 /// ============================================================================
 /// 
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+using TMPro;
 
 namespace Nothke.Utils
 {
     public static class RTUtils
     {
+        #region Quad creation
+
         static Mesh quad;
         public static Mesh GetQuad()
         {
@@ -50,28 +56,28 @@ namespace Nothke.Utils
 
             Vector3[] vertices = new Vector3[4]
             {
-            new Vector3(0, 0, 0),
-            new Vector3(width, 0, 0),
-            new Vector3(0, height, 0),
-            new Vector3(width, height, 0)
+                new Vector3(0, 0, 0),
+                new Vector3(width, 0, 0),
+                new Vector3(0, height, 0),
+                new Vector3(width, height, 0)
             };
             mesh.vertices = vertices;
 
             int[] tris = new int[6]
             {
-            // lower left triangle
-            0, 2, 1,
-            // upper right triangle
-            2, 3, 1
+                // lower left triangle
+                0, 2, 1,
+                // upper right triangle
+                2, 3, 1
             };
             mesh.triangles = tris;
 
             Vector2[] uv = new Vector2[4]
             {
-              new Vector2(0, 0),
-              new Vector2(1, 0),
-              new Vector2(0, 1),
-              new Vector2(1, 1)
+                new Vector2(0, 0),
+                new Vector2(1, 0),
+                new Vector2(0, 1),
+                new Vector2(1, 1)
             };
             mesh.uv = uv;
 
@@ -85,37 +91,156 @@ namespace Nothke.Utils
             if (blitShader)
                 return blitShader;
 
-            var shader = Shader.Find("Hidden/BlitQuad");
+            const string SHADER_NAME = "Sprites/Default";
+            var shader = Shader.Find(SHADER_NAME);
 
             if (!shader)
-                Debug.LogError("BlitTest shader not found, did you forget to include it in the project settings?");
+                Debug.LogError(SHADER_NAME + " shader not found, did you forget to include it in the project settings?");
 
             blitShader = shader;
             return blitShader;
         }
 
         static Material blitMaterial;
+
         public static Material GetBlitMaterial()
         {
             if (blitMaterial)
                 return blitMaterial;
 
+
             return new Material(GetBlitShader());
         }
 
-        const float zNear = 0.1f;
-        const float zFar = 100.0f;
-        const float zPos = -zFar / 2;
+        #endregion
 
-        public static void BlitQuad(RenderTexture rt, Material material, Vector2 position, Vector2 scale)
+        static RenderTexture prevRT;
+
+        public static void BeginOrthoRendering(this RenderTexture rt, float zBegin = -100, float zEnd = 100)
         {
-            Matrix4x4 objectMatrix = Matrix4x4.TRS(
-                new Vector3(position.x, position.y, zPos),
-                Quaternion.identity, scale);
+            // Create an orthographic matrix (for 2D rendering)
+            Matrix4x4 projectionMatrix = Matrix4x4.Ortho(0, 1, 0, 1, zBegin, zEnd);
 
+            rt.BeginRendering(projectionMatrix);
+        }
+
+        public static void BeginPixelRendering(this RenderTexture rt, float zBegin = -100, float zEnd = 100)
+        {
+            Matrix4x4 projectionMatrix = Matrix4x4.Ortho(0, rt.width, 0, rt.height, zBegin, zEnd);
+
+            rt.BeginRendering(projectionMatrix);
+        }
+
+        public static void BeginPerspectiveRendering(
+            this RenderTexture rt, float fov, in Vector3 position, in Quaternion rotation,
+            float zNear = 0.01f, float zFar = 1000f)
+        {
+            float aspect = (float)rt.width / rt.height;
+            Matrix4x4 projectionMatrix = Matrix4x4.Perspective(fov, aspect, zNear, zFar);
+            Matrix4x4 viewMatrix = Matrix4x4.TRS(position, rotation, new Vector3(1, 1, -1));
+
+            Matrix4x4 cameraMatrix = (projectionMatrix * viewMatrix.inverse);
+
+            rt.BeginRendering(cameraMatrix);
+        }
+
+        public static void BeginRendering(this RenderTexture rt, Matrix4x4 projectionMatrix)
+        {
+            // This fixes flickering (by @guycalledfrank)
+            // (because there's some switching back and forth between cameras, I don't fully understand)
+            if (Camera.current != null)
+                projectionMatrix *= Camera.current.worldToCameraMatrix.inverse;
+
+            // Remember the current texture and make our own active
+            prevRT = RenderTexture.active;
+            RenderTexture.active = rt;
+
+            // Push the projection matrix
+            GL.PushMatrix();
+            GL.LoadProjectionMatrix(projectionMatrix);
+        }
+
+        public static void EndRendering(this RenderTexture rt)
+        {
+            // Pop the projection matrix to set it back to the previous one
+            GL.PopMatrix();
+
+            // Revert culling
+            GL.invertCulling = false;
+
+            // Re-set the RenderTexture to the last used one
+            RenderTexture.active = prevRT;
+        }
+
+        public static void DrawMesh(this RenderTexture rt, Mesh mesh, Material material, in Matrix4x4 objectMatrix, int pass = 0)
+        {
+            bool canRender = material.SetPass(pass);
+
+            if (canRender)
+                Graphics.DrawMeshNow(mesh, objectMatrix);
+        }
+
+        public static void DrawTMPText(this RenderTexture rt, TMP_Text text, in Vector2 position, float size)
+        {
+            float aspect = (float)rt.width / rt.height;
+            Vector3 scale = new Vector3(size, size * aspect, 1);
+
+            Matrix4x4 matrix = Matrix4x4.TRS(position, Quaternion.identity, scale);
+
+            rt.DrawTMPText(text, matrix);
+        }
+
+        public static void DrawTMPText(this RenderTexture rt, TMP_Text text, in Matrix4x4 matrix)
+        {
+            Material material = text.fontSharedMaterial;
+            rt.DrawMesh(text.mesh, material, matrix);
+        }
+
+        #region Blit once functions
+
+        /// <summary>
+        /// Draws a mesh to render texture.
+        /// Position is defined in camera view 0-1 space where 0,0 is in bottom left corner.
+        /// <para>
+        /// For non-square textures, aspect ratio will be calculated so that the 0-1 space fits in the width.
+        /// Meaning that, for example, wider than square texture will have larger font size per texture area.
+        /// </para>
+        /// </summary>
+        /// <param name="rt"></param>
+        /// <param name="text"></param>
+        /// <param name="pos"></param>
+        /// <param name="size"></param>
+        /// <param name="clear"></param>
+        /// <param name="clearColor"></param>
+        public static void BlitTMPText(this RenderTexture rt, TMP_Text text, in Vector2 pos, float size,
+            bool clear = true, Color clearColor = default)
+        {
+            float aspect = (float)rt.width / rt.height;
+            Vector3 scale = new Vector3(size, size * aspect, 1);
+
+            Matrix4x4 matrix = Matrix4x4.TRS(pos, Quaternion.identity, scale);
+            BlitTMPText(rt, text, matrix, clear, clearColor);
+        }
+
+        public static void BlitTMPText(this RenderTexture rt, TMP_Text text, Matrix4x4 objectMatrix,
+            bool clear = true, Color clearColor = default)
+        {
+            Material mat = text.fontSharedMaterial;
+            BlitMesh(rt, objectMatrix, text.mesh, mat, clear, true, clearColor);
+        }
+
+        /// <summary>
+        /// Draws a mesh to render texture. The camera space is defined in normalized 0-1 coordinates, near and far planes are -100 and 100.
+        /// </summary>
+        /// <param name="objectMatrix">The model-matrix of the object</param>
+        /// <param name="invertCulling">In case the mesh renders inside-out, toggle this</param>
+        /// <param name="clear">Clears a texture to clearColor before drawing</param>
+        public static void BlitMesh(this RenderTexture rt, Matrix4x4 objectMatrix, Mesh mesh, Material material,
+            bool invertCulling = true, bool clear = true, Color clearColor = default)
+        {
             // Create an orthographic matrix (for 2D rendering)
             // You can otherwise use Matrix4x4.Perspective()
-            Matrix4x4 projectionMatrix = Matrix4x4.Ortho(0, 1, 0, 1, zNear, zFar);
+            Matrix4x4 projectionMatrix = Matrix4x4.Ortho(0, 1, 0, 1, -100, 100);
 
             // This fixes flickering (by @guycalledfrank)
             // (because there's some switching back and forth between cameras, I don't fully understand)
@@ -127,37 +252,56 @@ namespace Nothke.Utils
             RenderTexture.active = rt;
 
             // Set material as "active". Without this, Unity editor will freeze.
-            material.SetPass(0);
+            bool canRender = material.SetPass(0);
 
             // Push the projection matrix
             GL.PushMatrix();
             GL.LoadProjectionMatrix(projectionMatrix);
 
             // It seems that the faces are in a wrong order, so we need to flip them
-            GL.invertCulling = true;
+            GL.invertCulling = invertCulling;
 
             // Clear the texture
-            //GL.Clear(true, true, Color.black);
+            if (clear)
+                GL.Clear(true, true, clearColor);
 
             // Draw the mesh!
-            Graphics.DrawMeshNow(GetQuad(), objectMatrix);
+            if (canRender)
+                Graphics.DrawMeshNow(mesh, objectMatrix);
 
             // Pop the projection matrix to set it back to the previous one
             GL.PopMatrix();
+
+            // Revert culling
             GL.invertCulling = false;
 
             // Re-set the RenderTexture to the last used one
             RenderTexture.active = prevRT;
         }
 
-        public static void BlitSprite(RenderTexture rt, Texture texture, Vector2 position, Vector2 scale)
+        #endregion
+
+        public static void DrawQuad(this RenderTexture rt, Material material, in Rect rect)
+        {
+            Matrix4x4 objectMatrix = Matrix4x4.TRS(
+                rect.position, Quaternion.identity, rect.size);
+
+            rt.DrawMesh(GetQuad(), material, objectMatrix);
+
+            //GL.invertCulling = true;
+        }
+
+        public static void DrawSprite(this RenderTexture rt, Texture texture, in Rect rect)
         {
             Material material = GetBlitMaterial();
             material.mainTexture = texture;
-            //material.SetTexture("_TargetTex", texture);
 
-            BlitQuad(rt, material, position, scale);
+            DrawQuad(rt, material, rect);
         }
+
+        #region Utils
+
+        public static float Aspect(this Texture rt) => (float)rt.width / rt.height;
 
         public static Texture2D ConvertToTexture2D(this RenderTexture rt,
             TextureFormat format = TextureFormat.RGB24,
@@ -181,5 +325,7 @@ namespace Nothke.Utils
         {
             GUI.DrawTexture(new Rect(0, 0, texture.width, texture.height), texture);
         }
+
+        #endregion
     }
 }
